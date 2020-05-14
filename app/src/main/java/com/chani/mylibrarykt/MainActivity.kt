9582d6a9e873f836df59aa4e8f954e6a2527519e
LEEWOOCHAN <dncks1525@outlook.com>
2020-05-14 23:04:55 +0900

    First Commit

--- app/src/main/java/com/chani/mylibrarykt/MainActivity.kt
+++ app/src/main/java/com/chani/mylibrarykt/MainActivity.kt
@@ -2,12 +2,24 @@ package com.chani.mylibrarykt
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        test()
+    }
+
+    fun test() = runBlocking {
+        GlobalScope.launch {
+            repeat(1000) { i ->
+                println("I'm sleeping $i ...")
+                delay(500L)
+            }
+        }
+        delay(1300L)
     }
 }
